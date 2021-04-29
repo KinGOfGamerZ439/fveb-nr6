ffb34a30b457735cf7f545320a02ef906bcb7b7f
KinGOfGamerZ439 <77156036+KinGOfGamerZ439@users.noreply.github.com>
2021-04-29 21:58:51 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,58 @@
+player1_name = localStorage.getItem("number1");
+player2_name = localStorage.getItem("number2");
+
+player1_score=0;
+player2_score=0;
+
+document.getElementById("number1").innerHTML=number1_number+":";
+document.getElementById("number2").innerHTML=number2_number+":";
+
+document.getElementById("number1_score").innerHTML=number1_score;
+document.getElementById("number2_score").innerHTML=number2_score;
+
+function send()
+{
+    number1=document.getElementById("number1").value;
+    number2=document.getElementById("number2").value;
+    actual_answer=parseInt(number1)*parseInt(number2)
+
+
+    question_word = "<h4 id='word_display'>"+number1+"X"+number2+"</h4>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row =  question_word + input_box + check_button ; 
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
+}
+question_turn="player1";
+answer_turn="player2";
+
+function check()
+{
+    get_answer=document.getElementById("input_check_box").value;
+    
+    if(get_answer==actual_answer)
+    {
+        if(answer_turn=="player1")
+    {
+        update_player1_score=player1_score +1;
+        document.getElementById("player1_score").innerHTML=update_player1_score;
+    }
+    else
+    {
+        update_player2_score=player2_score +1;
+        document.getElementById("player2_score").innerHTML=update_player2_score;
+    }
+    }
+    if(question_turn=="player1")
+    {
+        question_turn="player2";
+        document.getElementById("player_question").innerHTML="question turn"+player2_name;
+    }
+    else
+    {
+        question_turn="player1";
+        document.getElementById("player_question").innerHTML="question turn"+player1_name;
+    }
+}
